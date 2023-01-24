15ff64b17913e96dc3b26888f55a9ac3054be57e
= <=>
2023-01-24 15:01:01 +0100

    added flags and overseas departments

--- query_handler.py
+++ query_handler.py
@@ -1,3 +1,4 @@
+import requests
 import pandas as pd
 import random
 import re
@@ -5,39 +6,45 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 import numpy as np
 import random
 
+
 class QueryHandler():
     def __init__(self, url="https://query.wikidata.org/sparql"):
         self.url = url
         self.sparql_wrapper = SPARQLWrapper(url)
         self.limit = 30
-        self.query = """SELECT DISTINCT ?item ?code_insee ?itemLabel WHERE 
+        self.query = """SELECT DISTINCT ?item ?code_insee ?itemLabel ?drapeau WHERE 
                         {
-                        ?item wdt:P31 wd:Q6465;
-                        wdt:P2586 ?code_insee.
+                        VALUES ?type {  wd:Q6465 wd:Q202216  }
+                        ?item wdt:P31 ?type;
+                        wdt:P2586 ?code_insee;
+                        wdt:P41 ?drapeau.
+                        FILTER NOT EXISTS { 
+                            FILTER(regex(str(?drapeau), "Flag%20of%20France" )) . 
+                        }
                         SERVICE wikibase:label { bd:serviceParam wikibase:language "fr" }
                         }
                         ORDER BY ?code_insee"""
 
-
-    def execute_query(self,query):
+    def execute_query(self, query):
         self.sparql_wrapper.setQuery(query)
         self.sparql_wrapper.setReturnFormat(JSON)
         results = self.sparql_wrapper.query().convert()
         df_res = pd.json_normalize(results['results']['bindings'])
         return df_res
 
-    def filter_properties(self,properties):
-        return [prop for prop in properties if prop.endswith("value") and prop != "item.value" and prop!="itemLabel.value"]
+    def filter_properties(self, properties):
+        return [prop for prop in properties if prop.endswith("value") and prop != "item.value" and prop != "itemLabel.value"]
 
-    def check_if_url(self,df,candidate,property):
-        name = list(df[df["item.value"] == candidate]["itemLabel.value"].drop_duplicates())[0]
-        if '//' in property or '//' in name :
+    def check_if_url(self, df, candidate, property):
+        name = list(df[df["item.value"] == candidate]
+                    ["itemLabel.value"].drop_duplicates())[0]
+        if '//' in property or '//' in name:
             return None
         return name
 
     def select_answer_and_options(self, df):
         ids = list(df["item.value"].drop_duplicates())
-        selected = False 
+        selected = False
         properties = self.filter_properties(df.columns)
         answer_prop = random.choice(properties)
         while not selected:
@@ -49,15 +56,22 @@ class QueryHandler():
             ids.remove(candidate_id)
 
             name = self.check_if_url(df,
-                                                candidate_id,
-                                                answer_prop)
+                                     candidate_id,
+                                     answer_prop)
             if name:
                 selected = True
-        answer = list(df[df["item.value"] == candidate_id][answer_prop].drop_duplicates())[0]
-        options = list(df[df["item.value"] != candidate_id][answer_prop].drop_duplicates())
+        answer = list(df[df["item.value"] == candidate_id]
+                      [answer_prop].drop_duplicates())[0]
+        options = list(df[df["item.value"] != candidate_id]
+                       [answer_prop].drop_duplicates())
         random.shuffle(options)
-        return name, answer_prop, answer,options[:3]
+        return name, answer_prop, answer, options[:3]
 
     def generate_question(self):
         res_df = self.execute_query(self.query)
         return self.select_answer_and_options(res_df)
+
+
+handler = QueryHandler()
+df_res = handler.execute_query(handler.query)
+df_res.to_csv('results.csv')
