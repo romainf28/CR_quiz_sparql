5d27aef359875d2958fde944d0b01b6d29b6bcdf
= <=>
2023-01-24 12:12:59 +0100

    aligned elements in interface

--- interface.py
+++ interface.py
@@ -30,13 +30,16 @@ class Quiz:
 
         self.radio_btns = self.radio_buttons()
 
+        self.current_question = Label(gui, text=self.question_list[self.question_number],
+                                      font=('ariel', 16, 'bold'), anchor='w')
+
         self.display_question()
 
         self.display_options()
 
     def radio_buttons(self):
         option_list = []
-        y_pos = 150
+        row = 2
 
         while len(option_list) < 4:
             radio_btn = Radiobutton(gui, text=" ", variable=self.selected_option,
@@ -44,28 +47,31 @@ class Quiz:
 
             option_list.append(radio_btn)
 
-            radio_btn.place(x=100, y=y_pos)
-            y_pos += 40
+            radio_btn.grid(row=row, column=1, columnspan=2, sticky="NW")
+
+            row += 1
 
         return option_list
 
     def next_button(self):
         next_btn = Button(gui, text="Question suivante", command=self.next_question,
-                          width=20, bg="blue", fg="white", font=('ariel', 16, 'bold'))
+                          bg="green", fg="white", font=('ariel', 16, 'bold'))
 
-        next_btn.place(x=350, y=380)
+        next_btn.grid(row=6, column=1, sticky="W")
 
     def quit_button(self):
         quit_btn = Button(gui, text="Quitter", command=gui.destroy,
-                          width=10, bg="black", fg="white", font=('ariel', 16, 'bold'))
+                          bg="red", fg="white", font=('ariel', 16, 'bold'))
 
-        quit_btn.place(x=700, y=50)
+        quit_btn.grid(row=6, column=2, sticky="W")
 
     def display_question(self):
-        question = Label(gui, text=self.question_list[self.question_number],
-                         width=60, font=('ariel', 16, 'bold'), anchor='w')
+        self.current_question.grid_remove()
 
-        question.place(x=70, y=100)
+        self.current_question = Label(gui, text=self.question_list[self.question_number],
+                                      font=('ariel', 16, 'bold'), anchor='w')
+
+        self.current_question.grid(row=1, column=1, columnspan=2, sticky="W")
 
     def display_options(self):
         i = 0
@@ -76,18 +82,19 @@ class Quiz:
             i += 1
 
     def next_question(self):
-        if self.check_answer(self.question_number):
-            self.nb_correct += 1
+        if self.selected_option.get() > 0:
+            if self.check_answer(self.question_number):
+                self.nb_correct += 1
 
-        self.question_number += 1
+            self.question_number += 1
 
-        if self.question_number == self.nb_questions:
-            self.summary()
-            gui.destroy()
+            if self.question_number == self.nb_questions:
+                self.summary()
+                gui.destroy()
 
-        else:
-            self.display_question()
-            self.display_options()
+            else:
+                self.display_question()
+                self.display_options()
 
     def check_answer(self, question_number):
         return self.option_list[self.question_number][self.selected_option.get()-1] == self.answers[question_number]
@@ -98,8 +105,8 @@ class Quiz:
 
     def display_title(self):
         title = Label(gui, text='Quiz sur les départements français',
-                      width=50, bg='green', fg='white', font=('ariel', 20, 'bold'))
-        title.place(x=0, y=2)
+                      bg='green', fg='white', font=('ariel', 20, 'bold'))
+        title.grid(row=0, column=0, columnspan=3, sticky="NSEW")
 
 
 gui = Tk()
@@ -107,6 +114,20 @@ gui = Tk()
 gui.geometry("800x450")
 
 gui.title("Quiz sur les départements français")
+
+Grid.rowconfigure(gui, 0, weight=4)
+Grid.columnconfigure(gui, 0, weight=20)
+
+Grid.rowconfigure(gui, 1, weight=8)
+Grid.columnconfigure(gui, 1, weight=5)
+
+Grid.rowconfigure(gui, 2, weight=4)
+Grid.columnconfigure(gui, 2, weight=55)
+
+Grid.rowconfigure(gui, 3, weight=4)
+Grid.rowconfigure(gui, 4, weight=4)
+Grid.rowconfigure(gui, 5, weight=4)
+Grid.rowconfigure(gui, 6, weight=17)
 
 quiz = Quiz()
 
